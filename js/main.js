eae7baa252d59f8e6cb84da92398598d4be3e49c
Edward Danilyuk <ask@edwardvdesign.com>
2017-12-01 02:10:45 -0500

    Added more advanced random generator.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 //First section generates a random number between 0 and 100.
 
 console.log(Math.floor(Math.random() * 100) + 1);
+
+/* More Advanced Generator
+
+Takes user input for range: min and max
+
+Generates a number between min and max
+
+*/
+function getRandomNumber(lower, upper) {
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
