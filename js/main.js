a80c5ca71f9b624569dd8f0af85e9aadbcd2151b
Edward Danilyuk <ask@edwardvdesign.com>
2017-12-01 02:16:24 -0500

    Added test to make sure inputs are numbers.

--- js/main.js
+++ js/main.js
@@ -10,5 +10,11 @@ Generates a number between min and max
 
 */
 function getRandomNumber(lower, upper) {
+  
+// Test to make sure lower and upper are numbers
+  if (isNaN(lower) || isNaN(upper) {
+      throw new Error('One of the inputed values is not a number');
+  }
+  
   return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
